1d1a9b3ce8defe23aac0b21f8879655bb6c886b2
Florian Holl <florian.holl@triaz.de>
2022-04-04 22:34:20 +0200

    Added navigation bar.

--- app/src/main/java/com/example/foodreminderapp/MainActivity.kt
+++ app/src/main/java/com/example/foodreminderapp/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.foodreminderapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
@@ -14,9 +17,16 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         super.onCreate(savedInstanceState)
 
         val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            .findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
         navController = navHostFragment.navController
         setupActionBarWithNavController(this, navController)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(
+            R.id.bottom_navigation_view
+        )
+
+        bottomNavigationView.setupWithNavController(navController)
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
